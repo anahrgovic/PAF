011adb249437b44e779bb0df3a22dff25840a0f6
anahrgovic <ahrgovic@pmfst.hr>
2021-03-15 21:48:59 +0100

    Vjezbe 2 zad 2

---
+++ Vjezbe_2/zad2.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+from math import sin, cos
+
+v0=float(input('Unesi iznos početne brzine:'))
+theta=float(input('Unesi iznos kuta otklona:'))
+
+vx=v0*cos(theta)
+vy=v0*sin(theta)
+y=0
+x=0
+t=0
+dt=0.5
+g=9.81
+pomak_x=[]
+pomak_y=[]
+vrijeme=[]
+
+for i in range(20):
+    x= vx*t
+    y = vy*t-0.5*g*(t**2)
+    t += dt
+    pomak_x.append(x)
+    pomak_y.append(y)
+    vrijeme.append(t)
+    
+plt.subplot(311)
+plt.plot(pomak_x,pomak_y)
+plt.subplot(312)
+plt.plot(vrijeme,pomak_x)
+plt.subplot(313)
+plt.plot(vrijeme, pomak_y)
+plt.show() 
