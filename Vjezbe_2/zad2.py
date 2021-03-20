0fcac25d1426e6e7d50ee5eb78c711eb8f2d1c3a
anahrgovic <ahrgovic@pmfst.hr>
2021-03-20 17:11:07 +0100

    Ispravljeni zadatak

--- Vjezbe_2/zad2.py
+++ Vjezbe_2/zad2.py
@@ -9,15 +9,15 @@ vy=v0*sin(theta)
 y=0
 x=0
 t=0
-dt=0.5
+dt=0.1
 g=9.81
 pomak_x=[]
 pomak_y=[]
 vrijeme=[]
 
 for i in range(20):
-    x= vx*t
-    y = vy*t-0.5*g*(t**2)
+    x = x + v*dt
+    v= v + a*dt
     t += dt
     pomak_x.append(x)
     pomak_y.append(y)
