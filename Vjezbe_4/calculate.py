64a34576ad8d258e585534c57eec27e1ace70897
anahrgovic <ahrgovic@pmfst.hr>
2021-04-11 23:10:57 +0200

    Vjezbe 4 završeno, ne radi zad2?

--- Vjezbe_4/calculate.py
+++ Vjezbe_4/calculate.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-
+#Zadatak 1
 def f1(x):
     return 5*(x**3)-2*(x**2)+2*x-3
 x1_=-2
@@ -18,15 +18,55 @@ for x in xl_:
 
 def f2(x):
     return math.sin(x)
-print(calculus.deriv(f2,1,0.1))
-print(calculus.derivacija(f2,1,0,0.1))
+#print(calculus.deriv(f2,1,0.1))
+#print(calculus.derivacija(f2,1,0,0.1))
 
 #print(calculus.deriv(f1,1,0.01))
 #print(calculus.deriv(f1,1,0.01))
 #print(calculus.derivacija(f1,1,5,0.1))
 
-a,b=calculus.derivacija(f2,-2,10,0.1)
+#a,b=calculus.derivacija(f2,-2,10,0.1)
 #plt.plot(xl_,dl_)
-plt.scatter(a,b, s=10, c="r")
+#plt.scatter(a,b, s=10, c="r")
 #plt.plot(a,b)
+#plt.show()
+
+
+#Zadatak 2
+
+a=0
+b=10
+def f1(x):
+    return 2*x*x-3
+def i_f1(x):
+    return (2/3)*(x*x*x)-3*x
+
+gornja_l=[]
+donja_l=[]
+analiticko_r=[]
+trapezno=[]
+d_l=[]
+
+dx=50
+for i in range(1,20):
+    d=dx*i
+    d_l.append(d)
+for d in d_l:
+    g_m,d_m=calculus.integr(f1,a,b,d)
+    trapez=calculus.integr_tr(f1,a,b,d)
+    analit=i_f1(b)-i_f1(a)
+    gornja_l.append(g_m)
+    donja_l.append(d_m)
+    trapezno.append(trapez)
+    analiticko_r.append(analit)
+
+
+
+#print(calculus.integr(f1,a,b,1))
+#print(calculus.integr_tr(f1,a,b,1))
+#plt.plot()
+plt.plot(d_l,gornja_l, '.')
+plt.plot(d_l,donja_l, '.')
+plt.plot(d_l,trapezno, '.')
+plt.plot(d_l,analiticko_r)
 plt.show()
--- Vjezbe_4/calculus.py
+++ Vjezbe_4/calculus.py
@@ -12,5 +12,28 @@ def derivacija(func,x1,x2,h):
         lista.append(d)
     return xlista,lista
 
+def integr(func,a,b,N):
+    dx=(b-a)/N
+    gornja=0
+    donja=0
+    x_gornja=a
+    x_donja=a+dx
+    for i in range(N):
+        gornja +=func(x_gornja)*dx
+        donja +=func(x_donja)*dx
+        x_gornja +=dx
+        x_donja +=dx
+    return x_gornja, x_donja
+
+def integr_tr(func,a,b,N):
+    dx=(b-a)/N
+    x=a
+    suma=0
+    for i in range(N):
+        suma += (func(x)+func(x+dx))
+        x +=dx
+        integral= (dx/2)*suma
+    return integral
+  
 
     
