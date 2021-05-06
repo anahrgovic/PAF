824fe0003a375bcf93aafb9f557d6269795b5915
Anahrgovic <ahrgovic@pmfst.hr>
2021-05-06 09:39:09 +0200

    Vjezbe 7 zad 1 nedovrseno

--- Vjezbe_7/1.cpp
+++ Vjezbe_7/1.cpp
@@ -1,20 +1,3 @@
-class Particle {
-    public:
-        double v;
-        double theta;
-        double x;
-        double y; 
-
-        double calculateArea(){   
-            return length * breadth;
-        }
-
-        double calculateVolume(){   
-            return length * breadth * height;
-        }
-
-};
-
 #include <iostream>
 #include <math.h>
 #include <list>
@@ -22,44 +5,54 @@ class Particle {
 
 using namespace std;
 
-class Particle {
+class Particle 
+{
     public:
-        Particle p1
         double v;
         double theta;
         double x;
-        double y; 
+        double y;
+        double vy;
+        double vx;
+        Particle(double v_, double theta_, double x_, double y_){
+            double pi = 3.14159265359;
+            v = v_;
+            theta = theta_ * (pi/180) ;
+            x = x_;
+            y = y_;
+            vx=v*cos(theta);
+            vy=v*sin(theta);
+        }
 
     private:
-        double __move(dt){
-            double vx;
-            vx=v*cos(theta);
-            double vy;
-            vy=v*sin(theta);
+        double __move(double dt){
+            
             float g=9.81;
             x=x + vx*dt;
             vy=vy - g*dt;
             y=y + vy*dt;
+            std::cout << vy <<  " " << y << std::endl;
         }
     public:
-         double range(){
-            while (y>=0){
-                p1.__move(dt);
-                if y <=0:
-                    break  
-            }
+         double range(double dt){
+            do {
+                 __move(dt);
                 
-            return max(x)
-            std::cout << max(x) << std::endl;
-         }
-        
-        
-        
-        
-    int main()
- {
-    Particle(1,2,3,4);
-    p1.__move(0.5)
-    p1.range()
-
+            }
+            while (y>=0);
+            
+            std::cout << x << std::endl;
+            }
 };
+        
+        
+        
+        
+int main()
+{
+    Particle p1(10,60,0,0);
+    //p1.__move(0.5);  
+    p1.range(0.01);    
+
+    return 0;  
+}
